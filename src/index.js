40cce7ecfcf0675decb8faaf2af41e3ac409d579
nnsahu02 <nnsahu2022@gmail.com>
2023-02-13 18:01:56 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const router = require('./routes')
 const app = express()
 
 app.use(express.json())
-
-mongoose.set('strictQuery', false)
-
-mongoose.connect("mongodb+srv://nnsahu2022:Sahurk012@mycluster.ne522qz.mongodb.net/sceduler", {
-    useNewUrlParser: true
-})
-    .then(() => console.log("MongoDB is connected."))
-    .catch(err => console.log(err))
 
 app.use('/', router)
 
